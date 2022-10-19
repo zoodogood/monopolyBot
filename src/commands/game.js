7dd5e6e872cc3c58b2902f0265efcf122f46e378
zoodogood <zoodogood@gmail.com>
2022-10-19 18:37:36 +0300

    First command

---
+++ src/commands/game.js
@@ -0,0 +1,11 @@
+import BaseCommand from '#lib/baseCommand.js';
+
+class Command extends BaseCommand {
+  static data = {
+    name: "game",
+    description: "Launch a game"
+  }
+}
+
+
+export { Command };
